2dfb36825c03aa74f4355c68cb2e69342af30597
namuHlaeR <namuhlaer@gmail.com>
2021-04-20 19:35:16 +0200

    first commit

---
+++ 1st/app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rbga(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    // get rand nr 0 - 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
---
+++ 1st/hex.js
@@ -0,0 +1,24 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+const textColor = document.getElementById('textColor');
+
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+    color.style.color = hexColor;    
+    textColor.style.color = hexColor;
+    //document.span.color = 'red';
+    //document.getElementsById('textColor').style.color = 'red';                                          
+
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+
+};
