400a32de450ddce45a91abda775e0577eb6da46f
namuHlaeR <namuhlaer@gmail.com>
2021-04-21 15:08:58 +0200

    Changes

--- 5th/app.js
+++ 5th/app.js
@@ -1,4 +1,28 @@
+const xlabels = [];
+
 getData();
+
+function chartIt() {
+
+const ctx = document.getElementById('chart').getContext('2d');
+const Chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: xlabels,
+        datasets: [{
+            label: 'Global average temperature',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+ });
+}
 
 async function getData(){
     const response = await fetch("ZonAnn.Ts+dSST.csv");
@@ -8,9 +32,11 @@ async function getData(){
     table.forEach(row => {
         const columns = row.split(',');
         const year = columns[0];
+        xlabels.push(year);
         const temp = columns[1];
         console.log(year, temp);
     })
-    
+
 };
 
+     
