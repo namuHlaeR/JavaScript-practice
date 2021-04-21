eb80b7e0ed7751b9e37266d998b8f75a6fca936e
namuHlaeR <namuhlaer@gmail.com>
2021-04-21 11:49:06 +0200

    Finished functionality

--- 3rd/app.js
+++ 3rd/app.js
@@ -1,17 +1,54 @@
 const character = document.getElementById("character-img");
 const object = document.getElementById("object-img");
-const counter = 0;
+const score = document.getElementById("score");
+let counter = 0;
 
 //Geeting click action for jump
 document.body.addEventListener("click", function jump(){
 
-    //Ckecking if animation is already applied
+//Ckecking if animation is already applied
     if(character.classList == "animate"){return}
 
-    //Adding and removing the animation 
+//Adding and removing the animation 
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate")
     }, 2000);
 });
 
+
+//Checking if we git the obstacle
+const survival = setInterval( function() {
+
+//Asking for output to compare
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let objectLeft = parseInt(window.getComputedStyle(object).getPropertyValue("left"));
+//Checking the merging point
+    if(objectLeft < 20 && objectLeft >- 20 && characterTop >= 130){
+        object.style.animation = "none";
+        alert(`Game Over! Score: ${Math.floor(counter/100)}`);
+        object.style = "block 5s infinite linear";
+//Reseting the counter
+        counter = 0
+        }
+    //Point counter
+        else{
+            scoreCounter(score.innerHTML = Math.floor(counter / 100));
+       }
+}, 10);
+
+//Point counter
+function scoreCounter(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let objectLeft = parseInt(window.getComputedStyle(object).getPropertyValue("left"));
+
+    //Setting counting point
+    if(objectLeft < 20 && objectLeft >- 20 && characterTop >= 130){
+    }
+
+//Adding point
+    else{
+        score.innerHTML = Math.floor(counter / 100);
+        counter++;
+    }
+}
