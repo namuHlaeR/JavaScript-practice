3379ffdbdc844294b990bd5241c42a9f4135c0a0
namuHlaeR <namuhlaer@gmail.com>
2021-04-21 10:57:12 +0200

    changes for functionality

--- 3rd/app.js
+++ 3rd/app.js
@@ -1,28 +1,17 @@
 const character = document.getElementById("character-img");
-const object = document.getElementById("object");
+const object = document.getElementById("object-img");
 const counter = 0;
 
-function jump(){
-    if(character.classList == "animation"){return};
-    character.classList.add( "animate");
+//Geeting click action for jump
+document.body.addEventListener("click", function jump(){
+
+    //Ckecking if animation is already applied
+    if(character.classList == "animate"){return}
+
+    //Adding and removing the animation 
+    character.classList.add("animate");
     setTimeout(function(){
-        character.classList.remove("animation");
-    },300);
-};
+        character.classList.remove("animate")
+    }, 2000);
+});
 
-let checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let objectLeft = parseInt(window.getComputedStyle(object).getPropertyValue("left"));;
-    if (objectLeft < 20 && objectLeft >- 20 && characterTop >- 130){
-        object.style.animation = "none";
-        alert("Game over SCORE: "+ Math.floor(counter / 100));
-        counter = 0;
-        object.style.animation = "block 1s infinite linear";}
-        else{
-            counter++;
-            document.getElementById("score").innerHTML = Math.floor(counter/100);
-
-        }   
-}, 100);
-
-console.log("hello")
